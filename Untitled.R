1c025e7696c0ab3bab6d9e807f7fbfe1b988fd5e
BR Hoover <brhoover@bren.ucsb.edu>
2019-03-12 12:49:37 -0700

    Added a space close #2

--- Untitled.R
+++ Untitled.R
@@ -3,4 +3,4 @@
 old_data<-read.csv(here("raw_data", "iris.csv"))
 new_data<-read.csv(here("raw_data", "iris_mythica.csv"))
 data<-rbind(old_data, new_data)
-write.csv(data, file="iris_four.csv")
+write.csv(data, file="iris_four.csv") 
